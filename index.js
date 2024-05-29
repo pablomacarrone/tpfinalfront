0f983b7ca5970d448c38cbac2078be105c971bed
pablomacarrone <168031916+pablomacarrone@users.noreply.github.com>
2024-05-29 14:44:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const btnAnterior = document.getElementById("btnAnterior");
+const btnSiguiente = document.getElementById("btnSiguiente");
+const contenedor = document.getElementById("contenedor");
+
+let pagina = 1;
+
+btnAnterior.addEventListener("click", () => {
+    if (pagina > 1) {
+        pagina -= 1;
+        cargarPeliculas();
+    }
+});
+
+btnSiguiente.addEventListener("click", () => {
+    if (pagina < 1000) {
+        pagina += 1;
+        cargarPeliculas();
+    }
+});
+
+const cargarPeliculas = async () => {
+    try {
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=191528030c357419329af1198edbcb24&language=es-MX&page=${pagina}`);
+        console.log(respuesta);
+
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+            console.log(datos);
+
+            let peliculas = '';
+
+            datos.results.forEach(pelicula => {
+                peliculas += `
+                <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <div class="card pelicula" style="width: 100%; margin: 10px;">
+                        <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="${pelicula.title}">
+                        <div class="card-body">
+                            <h5 class="card-title titulos">${pelicula.title}</h5>
+                            <div class="card-text descripcion" style="max-height: 100px; overflow-y: auto;">${pelicula.overview}</div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            });
+
+            contenedor.innerHTML = peliculas;
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+cargarPeliculas();
